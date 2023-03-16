f1af7781e7af488eafd6bfb5edd843630fdf7666
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-16 15:07:20 +0100

    Advanced Task Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Return: Always 0 (Success)
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
-	putchar("\"and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, sizeof(message) - 1);
 	return (1);
 }
