8dfe0abc93775eb044133d283c7f8b6a5806d06a
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-16 14:27:38 +0100

    Mandatory task completed :)

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */	
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art, ");
-	return 0;
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
