b62f21342f46af9f97cb564f26823abc3904713f
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-16 15:14:35 +0100

    Advanced Task Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,5 @@
-#include <stdio.h>
-/**
- *main - Entry point
- *Return: Always 1 (Success)
- */
+#include <unistd.h>
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, sizeof(message) - 1);
-	return (1);
+	return write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) == -1;
 }
