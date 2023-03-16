d5b8e428b5b480bc5f60f7e46c19e20ba2dcec95
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-16 15:20:55 +0100

    Advanced Task Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 int main(void)
 {
-	return write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
-		return 1;
+	return write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) == 1
 }
