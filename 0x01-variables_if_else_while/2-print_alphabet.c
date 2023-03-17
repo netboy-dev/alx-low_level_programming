9bf8d98bc67cb44cd13053172b4a34c7f35b7d1b
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-17 15:31:27 +0100

    Mandatory task completed :)

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- *Description: using the main function
+ *main - main where 
+ *the execution of this program starts
+ *Description: Get a random number and check its last digit, compare it with 5
  *Return: 0
  */
 int main(void)
@@ -8,6 +10,6 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
-		printf("%c",c);
+		putchar("%c" ,c);
 	return (0);
 }
