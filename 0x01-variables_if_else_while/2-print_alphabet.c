32ebe37939ff2865b3166e8ff40dc341c4684463
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-17 15:26:03 +0100

    Mandatory task completed :)

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+**/
+ *Description: using the main function
+ *Return: 0
+ */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
-		printf("%c", c);
+		printf("%c",c);
 	return (0);
 }
