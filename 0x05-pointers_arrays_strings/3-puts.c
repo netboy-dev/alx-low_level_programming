caa48e7988bf0f97d4b4d517fb0fb6d1ec052f2b
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-27 08:50:22 +0100

    New commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include <unistd.h>
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * _puts - Prints a string to standard output
+ * @str: Pointer to the string to be printed
  *
  * Return: void
  */
 void _puts(char *str)
 {
-        int len = 0;
+	int len = 0;
 
-        while (str[len] != '\0')
-                len++;
-
-        write(STDOUT_FILENO, str, len);
-        write(STDOUT_FILENO, "\n", 1);
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	write(STDOUT_FILENO, str, len);
+	write(STDOUT_FILENO, "\n", 1);
 }
