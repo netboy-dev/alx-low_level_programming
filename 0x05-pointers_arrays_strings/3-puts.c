97e7a15b22f8d531f08f8248d06c682e1304d715
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-27 08:42:42 +0100

    New commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,11 @@
  */
 void _puts(char *str)
 {
-    int len = 0;
+        int len = 0;
 
-    while (str[len] != '\0')
-        len++;
+        while (str[len] != '\0')
+                len++;
 
-    write(STDOUT_FILENO, str, len);
-    write(STDOUT_FILENO, "\n", 1);
+        write(STDOUT_FILENO, str, len);
+        write(STDOUT_FILENO, "\n", 1);
 }
