c41830ce4ac77c07130576edc876c11e19f19e1e
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-27 22:24:42 +0100

    New commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,44 @@
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random valid password for 101-crackme
+ * Return: Always 0 (Success)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 12
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random valid password for 101-crackme
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
-	int i;
-	srand(time(NULL));
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		password[i] = rand() % 94 + 32; // Generate ASCII printable characters
-						//
-						
-	}
-	password[PASSWORD_LENGTH] = '\0';
+        int i, sum, n;
+        char password[84];
 
-	printf("%s\n", password);
-	return 0;
+        srand(time(0));
+
+        sum = 0;
+        for (i = 0; sum < 2772; i++)
+        {
+                n = rand() % 62;
+                if (n < 10)
+                        password[i] = '0' + n;
+                else if (n < 36)
+                        password[i] = 'A' + n - 10;
+                else
+                        password[i] = 'a' + n - 36;
+                sum += password[i];
+        }
+
+        password[i] = 2772 - sum;
+        printf("%s", password);
+
+        return (0);
 }
 
