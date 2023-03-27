1e07d0587cd5934c33ba5d0d036da01e34b53101
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-27 07:12:19 +0100

    New commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: String to be measured
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+  int len = 0;
+
+  while (*s != '\0')
+  {
+    len++;
+    s++;
+  }
+
+  return (len);
+}
+
