2dff7a88854583bdc6041129092119dd00bbf1fe
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-27 07:13:57 +0100

    New commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,14 +7,12 @@
  */
 int _strlen(char *s)
 {
-  int len = 0;
-
-  while (*s != '\0')
-  {
-    len++;
-    s++;
-  }
-
-  return (len);
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
 
