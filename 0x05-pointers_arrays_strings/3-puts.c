cd823a29ba1c3a3fbad2baa680173d0ab176821b
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-03-27 08:35:33 +0100

    New commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string to standard output
+ * @str: Pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    write(STDOUT_FILENO, str, len);
+    write(STDOUT_FILENO, "\n", 1);
+}
+
