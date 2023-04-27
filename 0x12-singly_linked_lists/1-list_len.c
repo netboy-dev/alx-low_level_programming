06c1b80a6b8e0f6a476ae490b04ccf74e1843886
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-04-27 13:45:30 +0100

    BETTY ERROR FIXED

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,9 +3,9 @@
 /**
  * list_len - Returns the number of elements in a linked list_t list.
  * @h: A pointer to the list_t list.
- *
  * Return: The number of elements in the list.
  */
+
 size_t list_len(const list_t *h)
 {
     size_t len = 0;
@@ -18,4 +18,3 @@ size_t list_len(const list_t *h)
 
     return (len);
 }
-
