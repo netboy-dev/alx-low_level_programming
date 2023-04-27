fd0682bb0430c80e165ca4b780468200a76de779
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-04-27 13:48:53 +0100

    BETTY ERROR FIXED

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,21 @@
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a linked list_t list.
- * @h: A pointer to the list_t list.
- * Return: The number of elements in the list.
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t len = 0;
-	while (h != NULL)
+	size_t elements = 0;
+
+	while (h)
 	{
-		len++;
+		elements++;
 		h = h->next;
 	}
-	return (len);
+
+	return (elements);
 }
