aa5cd383966dd73ac2218409fc43a46ce46ea8c1
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-05-02 09:28:08 +0100

    PUSH + FIX

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- *pop_listint - deletes the head node of a linked list
- *@head: head of the list
- *
- *Return: the deleted node data
- */
-
-int pop_listint(listint_t **head)
-{
-	listint_t *popped;
-	int content;
-
-	if (*head == NULL)
-		return (0);
-
-	popped = *head;
-	content = popped->n;
-	free(popped);
-
-	*head = (*head)->next;
-	return (content);
-#include "lists.h"
-
-/**
  * pop_listint - returns data from and then deletes/frees memory allocated
  * to the first member of a struct list type listint_t
  *
